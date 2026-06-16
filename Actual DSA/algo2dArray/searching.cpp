fbf1c352988f77cfda13c95732629b87d98d5dfa
JAY SONI <jaysoni9549@gmail.com>
2026-06-16 12:16:48 +0530

    binary Search

---
+++ Actual DSA/algo2dArray/searching.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+int bruteSearch(int arr[][4],int m,int n,int key){              // O(m x n)
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==key){
+                cout<<i<<","<<j<<endl;
+            }
+        }
+    }
+    return -1;
+}
+
+int binarySearch(int arr[][4], int m, int n, int key){          // O(n x logm)
+    for(int i=0;i<m;i++){
+        int st = 0, end = n-1;
+        while(st<=end){
+            int mid = (st+end)/2;
+            if(arr[i][mid]==key){
+                cout<<i<<","<<mid<<endl;
+                return 0;
+            }else if(arr[i][mid]<key){
+                st = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int staircaseSearch(int arr[][4], int m, int n, int key){
+        int row=0,col=n-1;
+        while(row<m && col>=0){
+            if(arr[row][col] == key){
+                cout<<row<<","<<col<<endl;
+                return 0;
+            }else if(arr[row][col]<key){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return -1;
+    }
+int main(){
+    int m=5,n=4;
+    int key = 44;
+    int arr[5][4]={{10,20,30,40},
+                    {15,25,35,45},
+                    {27,29,37,48},
+                    {32,33,39,50},
+                    {42,44,45,64}};
+    bruteSearch(arr,m,n,key);
+    binarySearch(arr,m,n,key);
+    staircaseSearch(arr,m,n,key);
+}
--- basic C++/Array/04_array.cpp
+++ basic C++/Array/04_array.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int LinearSearch(int x[],int size, int target){
-    for(int i=0;i<size;i++){
+int LinearSearch(int x[],int n, int target){
+    for(int i=0;i<n;i++){
         if(x[i]==target){
             return i;
         }
@@ -10,9 +10,25 @@ int LinearSearch(int x[],int size, int target){
     return -1;
 }
 
+int BinarySearch(int x[],int n, int target){
+    int st=0 ,end = n-1;
+    while (st<=end){
+        int mid = (st+end)/2;
+        if(x[mid]==target){
+            return mid;
+        }else if(x[mid]<target){  // 2nd half
+            st = mid + 1;
+        }else{ // 1st half
+            end = mid - 1;
+        }
+    }
+    return -1;    
+}
+
 int main(){
     int x[] = {3,45,1,-3,4,9,76};
-    int size = 7;
-    int target = 8;
-    cout<<LinearSearch(x,size,target)<<endl;
+    int n = sizeof(x)/sizeof(int);
+    int target = -3;
+    cout<<LinearSearch(x,n,target)<<endl;
+    cout<<BinarySearch(x,n,target)<<endl;
 }
